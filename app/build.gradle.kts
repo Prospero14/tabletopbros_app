7555f8d494ca4838f4625ba91d169118f7f0cb7d
ProsperianSun <14dave@mail.ru>
2025-11-23 20:40:55 +0300

    40 round

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,8 @@ dependencies {
         implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
         
         implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
